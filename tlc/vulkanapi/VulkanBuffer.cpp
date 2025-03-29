c5ac4f263f6b5c48cc45b21d7f4bdb7d7e19997b
jaysmito <jaysmito101@gmail.com>
2025-03-29 08:39:21 +0530

    feat: better vulkan buffers

--- tlc/vulkanapi/VulkanBuffer.cpp
+++ tlc/vulkanapi/VulkanBuffer.cpp
@@ -1,32 +1,27 @@
 #include "vulkanapi/VulkanBuffer.hpp"
 #include "vulkanapi/VulkanDevice.hpp"
 
-
 namespace tlc
 {
 
-
-	VulkanBuffer::VulkanBuffer(VulkanDevice* device)
+	VulkanBuffer::VulkanBuffer(Raw<VulkanDevice> device, const VulkanBufferSettings &settings)
+		: m_Device(device), m_Settings(settings)
 	{
-		m_Device = device;
-		m_Size = 0;
+		if (!Recreate())
+		{
+			log::Warn("Failed to create Vulkan buffer");
+		}
 	}
 
 	VulkanBuffer::~VulkanBuffer()
 	{
-		Cleanup();
+		if (m_IsReady)
+		{
+			Cleanup();
+		}
 	}
 
-	void VulkanBuffer::Resize(Size size)
-	{
-		if (m_Size == size)
-			return;
-
-		m_Size = size;
-		RecreateBuffer();
-	}
-
-	Bool VulkanBuffer::SetData(const void* data, Size size, Size offset)
+	Bool VulkanBuffer::SetData(const void *data, Size size, Size offset)
 	{
 		if (size + offset > m_Size)
 		{
@@ -34,21 +29,17 @@ namespace tlc
 			return false;
 		}
 
-		void* mappedData = nullptr;
-		if (!MapMemory(&mappedData, size, offset))
+		if (!MapMemory(size, offset))
 		{
 			log::Error("VulkanBuffer::SetData: failed to map memory");
 			return false;
 		}
-
-		memcpy(mappedData, data, size);
-
+		memcpy(m_MappedData, data, size);
 		UnmapMemory();
-
 		return true;
 	}
 
-	Bool VulkanBuffer::GetData(void* data, Size size, Size offset)
+	Bool VulkanBuffer::GetData(void *data, Size size, Size offset)
 	{
 		if (size + offset > m_Size)
 		{
@@ -56,81 +47,99 @@ namespace tlc
 			return false;
 		}
 
-		void* mappedData = nullptr;
-		if (!MapMemory(&mappedData, size, offset))
+		if (!MapMemory(size, offset))
 		{
 			log::Error("VulkanBuffer::GetData: failed to map memory");
 			return false;
 		}
-
-		memcpy(data, mappedData, size);
-
+		memcpy(data, m_MappedData, size);
 		UnmapMemory();
-
 		return true;
 	}
 
-	Bool VulkanBuffer::MapMemory(void** data, Size size, Size offset)
+	Bool VulkanBuffer::MapMemory(Size size, Size offset, void **data)
 	{
 		if (size + offset > m_Size)
 		{
 			log::Error("VulkanBuffer::MapMemory: size + offset > m_Size");
 			return false;
 		}
-
-		auto result = m_Device->GetDevice().mapMemory(m_Memory, offset, size, vk::MemoryMapFlags(), data);
-
+		auto result = m_Device->GetDevice().mapMemory(m_Memory, offset, size, vk::MemoryMapFlags(), &m_MappedData);
 		if (result != vk::Result::eSuccess)
 		{
-			log::Error("VulkanBuffer::MapMemory: failed to map memory");
+			VkCall(result);
 			return false;
 		}
-
+		if (data != nullptr)
+		{
+			*data = m_MappedData;
+		}
+		m_MappedSize = size;
+		m_MappedOffset = offset;
 		return true;
 	}
 
 	void VulkanBuffer::UnmapMemory()
 	{
+		if (m_MappedData == nullptr)
+		{
+			log::Error("VulkanBuffer::UnmapMemory: memory is not mapped");
+			return;
+		}
 		m_Device->GetDevice().unmapMemory(m_Memory);
+		m_MappedData = nullptr;
+		m_MappedSize = 0;
+		m_MappedOffset = 0;
 	}
 
-
-	Bool VulkanBuffer::RecreateBuffer()
+	Bool VulkanBuffer::Recreate()
 	{
-		Cleanup();
+		if (m_IsReady)
+		{
+			log::Trace("VulkanBuffer::Recreate: buffer is already ready, cleaning up first");
+			Cleanup();
+		}
 
 		if (m_Size == 0)
-			return true;
+		{
+			log::Error("VulkanBuffer::Recreate: size is 0");
+			return false;
+		}
 
 		auto bufferInfo = vk::BufferCreateInfo()
-			.setSize(m_Size)
-			.setUsage(m_UsageFlags)
-			.setSharingMode(vk::SharingMode::eExclusive);
+							  .setSize(m_Settings.size)
+							  .setUsage(m_Settings.usage)
+							  .setSharingMode(m_Settings.sharingMode);
 
-		auto result = m_Device->GetDevice().createBuffer(&bufferInfo, nullptr, &m_Buffer);
-
-		if (result != vk::Result::eSuccess)
+		auto [bufferResult, buffer] = m_Device->GetDevice().createBuffer(bufferInfo);
+		if (bufferResult != vk::Result::eSuccess)
 		{
-			log::Error("VulkanBuffer::RecreateBuffer: failed to create buffer");
+			VkCall(bufferResult);
 			return false;
 		}
+		m_Buffer = buffer;
 
 		auto memRequirements = m_Device->GetDevice().getBufferMemoryRequirements(m_Buffer);
-
-		auto allocInfo = vk::MemoryAllocateInfo()
-			.setAllocationSize(memRequirements.size)
-			.setMemoryTypeIndex(m_Device->FindMemoryType(memRequirements.memoryTypeBits, m_MemoryPropertyFlags));
-
-		result = m_Device->GetDevice().allocateMemory(&allocInfo, nullptr, &m_Memory);
-
-		if (result != vk::Result::eSuccess)
-		{
-			log::Error("VulkanBuffer::RecreateBuffer: failed to allocate buffer memory");
-			return false;
+		if (m_Settings.useDeviceMemoryPool) {
+			log::Fatal("Device Memory Pool has not yet been implemented!");
 		}
+		else {
+			auto allocInfo = vk::MemoryAllocateInfo()
+								 .setAllocationSize(memRequirements.size)
+								 .setMemoryTypeIndex(m_Device->FindMemoryType(memRequirements.memoryTypeBits, m_Settings.memoryPropertyFlags));
 
+			auto [result, memory] = m_Device->GetDevice().allocateMemory(allocInfo);
+			if (result != vk::Result::eSuccess)
+			{
+				VkCall(result);
+				return false;
+			}
+			m_Memory = memory;
+		}
 		VkCall(m_Device->GetDevice().bindBufferMemory(m_Buffer, m_Memory, 0));
 
+		m_Size = m_Settings.size;
+		m_UsageFlags = m_Settings.usage;
 		m_IsReady = true;
 
 		return true;
@@ -138,12 +147,72 @@ namespace tlc
 
 	void VulkanBuffer::Cleanup()
 	{
-		if (!m_IsReady)
+		if (!m_IsReady) {
+			log::Trace("VulkanBuffer::Cleanup: buffer is not ready, cannot cleanup");
 			return;
+		}
+
+		if (m_MappedData != nullptr) {
+			UnmapMemory();
+		}
 
 		m_Device->WaitIdle();
+
 		m_Device->GetDevice().destroyBuffer(m_Buffer);
-		m_Device->GetDevice().freeMemory(m_Memory);
+
+		if (m_Settings.useDeviceMemoryPool) {
+			log::Fatal("Device Memory Pool has not yet been implemented!");
+		}
+		else {
+			m_Device->GetDevice().freeMemory(m_Memory);
+		}
+
 		m_IsReady = false;
+	}
+
+	Bool VulkanBuffer::Flush(Size size, Size offset) {
+		if (size + offset > m_Size) {
+			log::Error("VulkanBuffer::Flush: size + offset > m_Size");
+			return false;
+		}
+		
+		if (m_Settings.useDeviceMemoryPool) {
+			log::Fatal("Device Memory Pool has not yet been implemented!");
+		}
+		else {
+			auto mappedRange = vk::MappedMemoryRange()
+				.setMemory(m_Memory)
+				.setSize(size)
+				.setOffset(offset);
+			auto result = m_Device->GetDevice().flushMappedMemoryRanges(mappedRange);
+			if (result != vk::Result::eSuccess) {
+				VkCall(result);
+				return false;
+			}
+			return true;
+		}
+	}
+	
+	Bool VulkanBuffer::Invalidate(Size size, Size offset) {
+		if (size + offset > m_Size) {
+			log::Error("VulkanBuffer::Invalidate: size + offset > m_Size");
+			return false;
+		}
+		
+		if (m_Settings.useDeviceMemoryPool) {
+			log::Fatal("Device Memory Pool has not yet been implemented!");
+		}
+		else {
+			auto mappedRange = vk::MappedMemoryRange()
+				.setMemory(m_Memory)
+				.setSize(size)
+				.setOffset(offset);
+			auto result = m_Device->GetDevice().invalidateMappedMemoryRanges(mappedRange);
+			if (result != vk::Result::eSuccess) {
+				VkCall(result);
+				return false;
+			}
+			return true;
+		}
 	}
 }
--- tlc/vulkanapi/VulkanBuffer.hpp
+++ tlc/vulkanapi/VulkanBuffer.hpp
@@ -4,45 +4,85 @@
 
 namespace tlc
 {
-
 	class VulkanContext;
 	class VulkanDevice;
+
+	struct VulkanBufferSettings {
+		Size size = 0;
+		vk::BufferUsageFlags usage = vk::BufferUsageFlagBits::eTransferDst;
+		vk::MemoryPropertyFlags memoryPropertyFlags = vk::MemoryPropertyFlagBits::eDeviceLocal;
+		vk::SharingMode sharingMode = vk::SharingMode::eExclusive;
+		// Raw<VulkanDeviceMemoryPool> deviceMemoryPool = nullptr; // TODO
+        Bool useDeviceMemoryPool = false;
+
+		inline VulkanBufferSettings& SetSize(Size s) { size = s; return *this; }
+		inline VulkanBufferSettings& SetUsage(vk::BufferUsageFlags u) { usage = u; return *this; }
+		inline VulkanBufferSettings& SetMemoryPropertyFlags(vk::MemoryPropertyFlags flags) { memoryPropertyFlags = flags; return *this; }
+		inline VulkanBufferSettings& SetSharingMode(vk::SharingMode mode) { sharingMode = mode; return *this; }
+		inline VulkanBufferSettings& SetUseDeviceMemoryPool(Bool u) { useDeviceMemoryPool = u; return *this; }
+		// inline VulkanBufferSettings& SetDeviceMemoryPool(Raw<VulkanDeviceMemoryPool> p) { deviceMemoryPool = p; return *this; }
+	};
 
 	class VulkanBuffer
 	{
 	public:
-		VulkanBuffer(VulkanDevice* device);
+		VulkanBuffer(Raw<VulkanDevice> device, const VulkanBufferSettings& settings = VulkanBufferSettings());
 		~VulkanBuffer();
-
-		void Resize(Size size);
-
 
 		Bool SetData(const void* data, Size size, Size offset = 0);
 		Bool GetData(void* data, Size size, Size offset = 0);
-		Bool MapMemory(void** data, Size size, Size offset = 0);
+		Bool MapMemory(Size size, Size offset, void** data = nullptr);
 		void UnmapMemory();
+		Bool Flush(Size size, Size offset = 0);
+		Bool Invalidate(Size size, Size offset = 0);
 
 		inline vk::Buffer GetBuffer() const { return m_Buffer; }
-		inline vk::DeviceMemory GetMemory() const { return m_Memory; }
 		inline bool IsReady() const { return m_IsReady; }
 		inline Size GetSize() const { return m_Size; }
+		inline vk::BufferUsageFlags GetUsageFlags() const { return m_UsageFlags; }
+		inline Size GetMappedSize() const { return m_MappedSize; }
+		inline Size GetMappedOffset() const { return m_MappedOffset; }
 
-		inline void SetUsageFlags(vk::BufferUsageFlags usageFlags) { m_UsageFlags = usageFlags; }
-		inline void SetMemoryPropertyFlags(vk::MemoryPropertyFlags memoryPropertyFlags) { m_MemoryPropertyFlags = memoryPropertyFlags; }
+		inline VulkanBufferSettings& GetSettings() { return m_Settings; }
 
+		inline void* GetMappedDataRaw() const { return m_MappedData; }
+
+		template<typename T>
+		inline Raw<T> GetMappedData() const { return static_cast<T*>(m_MappedData); }
+
+
+		inline static Ref<VulkanBuffer> CreateStagingBuffer(Raw<VulkanDevice> device, Size size) {
+			auto bufferSettings = VulkanBufferSettings()
+				.SetSize(size)
+				.SetUsage(vk::BufferUsageFlagBits::eTransferSrc)
+				.SetMemoryPropertyFlags(vk::MemoryPropertyFlagBits::eHostVisible | vk::MemoryPropertyFlagBits::eHostCoherent);
+			auto buffer = CreateRef<VulkanBuffer>(device, bufferSettings);
+			if (!buffer->IsReady()) {
+				log::Error("Failed to create staging buffer");
+				return nullptr;
+			}
+			return buffer;				
+		}
 
 	private:
-		Bool RecreateBuffer();
+		Bool Recreate();
 		void Cleanup();
 
 
 	private:
 		Raw<VulkanDevice> m_Device = nullptr;
-		vk::BufferUsageFlags m_UsageFlags = vk::BufferUsageFlagBits::eVertexBuffer;
-		vk::MemoryPropertyFlags m_MemoryPropertyFlags = vk::MemoryPropertyFlagBits::eHostVisible | vk::MemoryPropertyFlagBits::eHostCoherent;
+
+		VulkanBufferSettings m_Settings;
+
+		Size m_Size = 0;
+		vk::BufferUsageFlags m_UsageFlags = vk::BufferUsageFlagBits::eTransferDst;
+
+		void* m_MappedData = nullptr;
+		vk::DeviceSize m_MappedSize = 0;
+		vk::DeviceSize m_MappedOffset = 0;
+
 		vk::Buffer m_Buffer = VK_NULL_HANDLE;
 		vk::DeviceMemory m_Memory = VK_NULL_HANDLE;
-		Size m_Size = 0;
 		Bool m_IsReady = false;
 	};
 
