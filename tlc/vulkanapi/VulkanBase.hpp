9003581e07aace9d46c598a3b040ded6c29c9f77
Jaysmito101 <jaysmito101@gmail.com>
2025-12-06 17:16:03 +0530

    fix: correct comment and formatting for system header directives in VulkanBase.hpp

--- tlc/vulkanapi/VulkanBase.hpp
+++ tlc/vulkanapi/VulkanBase.hpp
@@ -6,7 +6,20 @@
 #define API_VERSION VK_API_VERSION_1_4
 #define VULKAN_HPP_NO_EXCEPTIONS
 #define VULKAN_HPP_ASSERT_ON_RESULT
+// if define this as a system header foer different compilers
+#if defined(__clang__)
+#pragma clang system_header
+#elif defined(__GNUC__) || defined(__GNUG__)
+#pragma GCC system_header
+#elif defined(_MSC_VER)
+#pragma warning(push, 0)
+#endif
+
 #include "vulkan/vulkan.hpp"
+
+#if defined(_MSC_VER)
+#pragma warning(pop)
+#endif
 
 #define VkCritCall(x)                                                                             \
     if (x != vk::Result::eSuccess) {                                                              \
